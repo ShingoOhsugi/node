48cf3564519aa2739618714d70758e13860120f6
ShingoOhsugi <ref.3570.40@gmail.com>
2014-04-06 23:25:44 +0900

    win-node連携完了

---
+++ WinClient/HtmlInterop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows;
+
+namespace PopClient
+{
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public class HtmlInterop
+    {
+        public void ScriptNotify(string reqValue)
+        {
+            // 引数のJson文字列をDictionaryに変換
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            Dictionary<string, object> reqDic = 
+                jss.Deserialize<Dictionary<string, object>>(reqValue);
+
+            MainWindow mw = ((MainWindow)Application.Current.MainWindow) as MainWindow;
+            mw._notifyIcon.BalloonTipTitle = reqDic["BalloonTipTitle"] as string;
+            mw._notifyIcon.BalloonTipText = reqDic["BalloonTipText"] as string;
+            mw._notifyIcon.ShowBalloonTip((int)reqDic["ShowBalloonTip"]);
+        }
+    }
+}
--- WinClient/MainWindow.xaml.cs
+++ WinClient/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,14 +23,34 @@ namespace PopClient
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
     public partial class MainWindow : Window
     {
-        private NotifyIcon _notifyIcon;
+        public NotifyIcon _notifyIcon;
 
 
         public MainWindow()
         {
             InitializeComponent();
+
+
+            //WebBrowserのレンダリングモードはデフォルトIE7であるため、HTML5が使えない。
+            //なのでIE9に変更する。
+            string FEATURE_BROWSER_EMULATION
+                = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+            string FEATURE_DOCUMENT_COMPATIBLE_MODE
+                = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_DOCUMENT_COMPATIBLE_MODE";
+            string exename = Process.GetCurrentProcess().MainModule.ModuleName;
+            using (RegistryKey regkey1 =
+                Registry.CurrentUser.CreateSubKey(FEATURE_BROWSER_EMULATION))
+            using (RegistryKey regkey2 =
+                Registry.CurrentUser.CreateSubKey(FEATURE_DOCUMENT_COMPATIBLE_MODE))
+            {
+                regkey1.SetValue(exename, 9000, RegistryValueKind.DWord);
+                regkey2.SetValue(exename, 90000, RegistryValueKind.DWord);
+                regkey1.Close();
+                regkey2.Close();
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -59,8 +81,8 @@ namespace PopClient
                 new System.Windows.Forms.MouseEventHandler(_notifyIcon_MouseClick);
 
 
-
-            wbMain.Navigate(new Uri("http://www.yahoo.co.jp"));
+            wbMain.ObjectForScripting = new HtmlInterop();
+            wbMain.Navigate(new Uri("http://localhost:8080/"));
         }
 
         private void exitItem_Click(object sender, EventArgs e)
@@ -74,8 +96,9 @@ namespace PopClient
             if (e.Button == MouseButtons.Left)
             {
                 //ウィンドウを可視化
-                Visibility = Visibility.Visible;
-                WindowState = WindowState.Normal;
+                this.Visibility = Visibility.Visible;
+                this.WindowState = WindowState.Normal;
+                this.Activate();
             }
         }
 
