bad4f073aa73e674c09cc4cbac3cd16b59a2338d
ShingoOhsugi <ref.3570.40@gmail.com>
2014-04-06 16:42:51 +0900

    タスクトレイに格納

--- WinClient/MainWindow.xaml.cs
+++ WinClient/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,6 +23,9 @@ namespace PopClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIcon _notifyIcon;
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,9 +33,62 @@ namespace PopClient
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            wbMain.NavigateToString("http://www.yahoo.co.jp");
-                        
+            //タスクバーに表示されないようにする
+            ShowInTaskbar = false;
+
+            //タスクトレイアイコンを初期化する
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Text = "HOTLINE by ndc";
+            _notifyIcon.Icon = new Icon("img/notifyIcon.ico");
+
+            //タスクトレイに表示する
+            _notifyIcon.Visible = true;
+
+            //アイコンにコンテキストメニュー「終了」を追加する
+            ContextMenuStrip menuStrip = new ContextMenuStrip();
+
+            ToolStripMenuItem exitItem = new ToolStripMenuItem();
+            exitItem.Text = "終了";
+            menuStrip.Items.Add(exitItem);
+            exitItem.Click += new EventHandler(exitItem_Click);
+
+            _notifyIcon.ContextMenuStrip = menuStrip;
+
+            //タスクトレイアイコンのクリックイベントハンドラを登録する
+            _notifyIcon.MouseClick += 
+                new System.Windows.Forms.MouseEventHandler(_notifyIcon_MouseClick);
+
+
+
+            wbMain.Navigate(new Uri("http://www.yahoo.co.jp"));
         }
+
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            _notifyIcon.Dispose();
+            System.Windows.Application.Current.Shutdown();
+        }
+        
+        private void _notifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //ウィンドウを可視化
+                Visibility = Visibility.Visible;
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            //閉じるのをキャンセルする
+            e.Cancel = true;
+
+            //ウィンドウを非可視にする
+            Visibility = Visibility.Collapsed;
+        }
+
+
 
 
     }
